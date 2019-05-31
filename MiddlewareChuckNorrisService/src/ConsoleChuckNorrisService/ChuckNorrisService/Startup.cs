843dc868d8828fd9942f0d49d4c145716e884699
KotelczukB <WhiteWolf12>
2019-05-31 11:42:48 +0200

    WS Ubung 2 MiddleWare

---
+++ MiddlewareChuckNorrisService/src/ConsoleChuckNorrisService/ChuckNorrisService/Startup.cs
@@ -0,0 +1,30 @@
+﻿using ChuckNorrisService.Providers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ChuckNorrisService
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var provider = new FileSystemJokeProvider();
+
+            app.Run(async context =>
+            {
+                var joke = await provider.GetRandomJokeAsync();
+                var raw = JsonConvert.SerializeObject(joke);
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                await context.Response.WriteAsync(raw);
+            });
+        }
+     }
+}
